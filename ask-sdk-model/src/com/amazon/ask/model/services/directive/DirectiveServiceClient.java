48a3844f1649a144a205ee9a480ecc518cdaef08
Alexa <>
2022-03-23 00:55:28 +0000

    Updating model definitions

--- ask-sdk-model/src/com/amazon/ask/model/services/directive/DirectiveServiceClient.java
+++ ask-sdk-model/src/com/amazon/ask/model/services/directive/DirectiveServiceClient.java
@@ -31,7 +31,7 @@ public class DirectiveServiceClient extends BaseServiceClient implements Directi
   private final UserAgentHelper userAgentHelper;
   public DirectiveServiceClient(ApiConfiguration apiConfiguration) {
       super(apiConfiguration);
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.39.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.39.1").build();
   }
 
   /**
--- ask-sdk-model/src/com/amazon/ask/model/services/ups/UpsServiceClient.java
+++ ask-sdk-model/src/com/amazon/ask/model/services/ups/UpsServiceClient.java
@@ -31,7 +31,7 @@ public class UpsServiceClient extends BaseServiceClient implements UpsService {
   private final UserAgentHelper userAgentHelper;
   public UpsServiceClient(ApiConfiguration apiConfiguration) {
       super(apiConfiguration);
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.39.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.39.1").build();
   }
 
   /**
