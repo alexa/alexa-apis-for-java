446bf359a42b9785bf07dd2602780a7962060ab6
ask-sdk <ask-sdk@amazon.com>
2023-01-28 15:35:16 +0000

    Updating model definitions

--- ask-sdk-model/src/com/amazon/ask/model/services/timerManagement/TimerManagementServiceClient.java
+++ ask-sdk-model/src/com/amazon/ask/model/services/timerManagement/TimerManagementServiceClient.java
@@ -31,7 +31,7 @@ public class TimerManagementServiceClient extends BaseServiceClient implements T
   private final UserAgentHelper userAgentHelper;
   public TimerManagementServiceClient(ApiConfiguration apiConfiguration) {
       super(apiConfiguration);
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.44.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.45.0").build();
   }
 
   /**
