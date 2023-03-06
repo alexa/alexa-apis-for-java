77fbbb94c0930bcd85dadc73252e42888f21b9a7
Alexa <>
2023-03-06 16:04:59 +0000

    Updating model definitions

--- ask-smapi-model/src/com/amazon/ask/model/services/skillManagement/SkillManagementServiceClient.java
+++ ask-smapi-model/src/com/amazon/ask/model/services/skillManagement/SkillManagementServiceClient.java
@@ -43,13 +43,13 @@ public class SkillManagementServiceClient extends BaseServiceClient implements S
                                     .withSerializer(apiConfiguration.getSerializer())
                                     .build())
           .build();
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.34.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.35.0").build();
   }
 
   public SkillManagementServiceClient(ApiConfiguration apiConfiguration, LwaClient lwaClient) {
       super(apiConfiguration);
       this.lwaClient = lwaClient;
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.34.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.35.0").build();
   }
 
   /**
