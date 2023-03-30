d038bbf82fd7a1ef579a8ed8224972dbee7b82f1
ask-sdk <ask-sdk@amazon.com>
2023-03-30 15:39:35 +0000

    Updating model definitions

--- ask-sdk-model/src/com/amazon/ask/model/services/directive/DirectiveServiceClient.java
+++ ask-sdk-model/src/com/amazon/ask/model/services/directive/DirectiveServiceClient.java
@@ -31,7 +31,7 @@ public class DirectiveServiceClient extends BaseServiceClient implements Directi
   private final UserAgentHelper userAgentHelper;
   public DirectiveServiceClient(ApiConfiguration apiConfiguration) {
       super(apiConfiguration);
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.62.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.63.0").build();
   }
 
   /**
