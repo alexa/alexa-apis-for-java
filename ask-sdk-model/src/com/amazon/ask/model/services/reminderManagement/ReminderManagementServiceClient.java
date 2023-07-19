3f678b079162caa71b821a534a31bf2e9ab4e64b
Alexa <>
2023-07-19 15:35:50 +0000

    Updating model definitions

--- ask-sdk-model/src/com/amazon/ask/model/services/reminderManagement/ReminderManagementServiceClient.java
+++ ask-sdk-model/src/com/amazon/ask/model/services/reminderManagement/ReminderManagementServiceClient.java
@@ -31,7 +31,7 @@ public class ReminderManagementServiceClient extends BaseServiceClient implement
   private final UserAgentHelper userAgentHelper;
   public ReminderManagementServiceClient(ApiConfiguration apiConfiguration) {
       super(apiConfiguration);
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.73.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.74.0").build();
   }
 
   /**
