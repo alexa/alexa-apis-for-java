a77c0e1c0b23675464995567eb7b2ab903e99b86
Alexa <>
2023-08-09 15:35:52 +0000

    Updating model definitions

--- ask-sdk-model/src/com/amazon/ask/model/services/endpointEnumeration/EndpointEnumerationServiceClient.java
+++ ask-sdk-model/src/com/amazon/ask/model/services/endpointEnumeration/EndpointEnumerationServiceClient.java
@@ -31,7 +31,7 @@ public class EndpointEnumerationServiceClient extends BaseServiceClient implemen
   private final UserAgentHelper userAgentHelper;
   public EndpointEnumerationServiceClient(ApiConfiguration apiConfiguration) {
       super(apiConfiguration);
-      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.77.0").build();
+      this.userAgentHelper = UserAgentHelper.builder().withSdkVersion("1.78.0").build();
   }
 
   /**
